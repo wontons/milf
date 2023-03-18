690bd3e0f1000d17bdc6f7433390be314ea99b48
Anton <antonsawka2@gmail.com>
2023-03-18 12:24:04 -0500

    update about us

--- frontend/src/components/AboutUs.tsx
+++ frontend/src/components/AboutUs.tsx
@@ -4,7 +4,7 @@ export function AboutUs() {
     return (
         <Box m="1rem" display="flex" flex={1} alignItems="top" justifyContent="center">
             <Typography variant="h6" width="50%" textAlign="center">
-            The 2023 Manitoba Tour aims to create a season long competition for players to play for prizes and glory. Tour members will be competing for points at each tour event to qualify for the Tour Championship at the end of the season. Players will earn points by placing well in their respective divisions, earning more the better they play. The top players at in each division at the end of the season will then battle one last time in the Tour Championship to crown a Champion and earn prizes.
+                Man I Love Frisbee aims to promote the sport of disc golf in Manitoba. Our goal is to help introduce the sport to new players and provide events and other opportunities to our rapidly growing community. We are excited to be a part of disc golf in Manitoba and help it grow!
             </Typography>
         </Box>
     );
